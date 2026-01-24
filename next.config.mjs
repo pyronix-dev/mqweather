cbb8469b3760e4e2fb6cd967ec51b4123247b3ce
Omar Rafik <admin0mrx@proton.me>
2026-01-24 16:22:07 +0100

    fix: whitelist stripe domains in CSP to resolve js loading error

--- next.config.mjs
+++ next.config.mjs
@@ -38,7 +38,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com https://api.maptiler.com https://unpkg.com; img-src 'self' blob: data: https://api.maptiler.com https://meteofrance.mq https://www.meteo.fr https://vigilance.meteofrance.fr https://raw.githubusercontent.com; font-src 'self' https://cdn.jsdelivr.net https://fonts.gstatic.com; connect-src 'self' https://api.open-meteo.com https://marine-api.open-meteo.com https://api.maptiler.com https://*.supabase.co https://api.cerebras.ai; worker-src 'self' blob:;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://va.vercel-scripts.com https://js.stripe.com; frame-src 'self' https://js.stripe.com https://hooks.stripe.com; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com https://api.maptiler.com https://unpkg.com; img-src 'self' blob: data: https://api.maptiler.com https://meteofrance.mq https://www.meteo.fr https://vigilance.meteofrance.fr https://raw.githubusercontent.com https://*.stripe.com; font-src 'self' https://cdn.jsdelivr.net https://fonts.gstatic.com; connect-src 'self' https://api.open-meteo.com https://marine-api.open-meteo.com https://api.maptiler.com https://*.supabase.co https://api.cerebras.ai https://api.stripe.com; worker-src 'self' blob:;"
           }
         ]
       }
